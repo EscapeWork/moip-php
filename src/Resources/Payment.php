ff40504430b868cc202076d9d865846bc9d1d1f2
Luis Dalmolin <luis.nh@gmail.com>
2016-01-20 14:45:40 -0200

    Init 0.2 version

---
+++ src/Resources/Payment.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EscapeWork\Moip\Resources;
+
+use EscapeWork\Moip\Exceptions\RemoteException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+class Payment extends Resource
+{
+
+    /**
+     * API endpoints
+     */
+    protected $endpoint = [
+        'production' => '',
+        'sandbox'    => 'https://sandbox.moip.com.br/orders/v2/',
+    ];
+
+    /**
+     * Models needed
+     */
+    protected $required = [
+        'customer',
+        'address',
+        'credit_card',
+        'plan',
+    ];
+}
